0eb3fc17a35d851a7e5d63fcc8f065d14accb131
Granger Pasko <granger@rachelcarlson-100-64-79-66.resnet.ucsc.edu>
2022-04-09 15:21:45 -0700

    made FindPath:

---
+++ pa2/FindPath.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "List.h"
+#include "Graph.h"
+
+int main(int argc, char argv[]){
+	if(argc != 3){
+		pritnf("Incorrect number of file arguments. Run 'Find Path <input file> <output file>'\n");
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
