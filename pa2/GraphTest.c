ff1d3ab38c54806e28c8b273b5d418bd30904ee5
Granger Pasko <granger@rachelcarlson-100-64-78-11.resnet.ucsc.edu>
2022-04-13 16:08:11 -0700

    anotha day another dolla

--- pa2/GraphTest.c
+++ pa2/GraphTest.c
@@ -36,9 +36,9 @@ int main(int argc, char* argv[]){
    G = newGraph(n);
    for(i=1; i<n; i++){
       if( i%7!=0 ) addEdge(G, i, i+1);
-      printf("edge added\n");
+      printf("edge %s -> %s added\n", i, i+1);
       if( i<=28  ) addEdge(G, i, i+7);
-      printf("edge added\n");
+      printf("edge %s -> %s added\n", i, i+7);
    }
    printf("all loop edges added\n");
    addEdge(G, 9, 31);
