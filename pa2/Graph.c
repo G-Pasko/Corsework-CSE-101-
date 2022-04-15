3091330f590b345349a0c694f6230320d1c26367
Granger Pasko <granger@rachelcarlson-100-64-76-108.resnet.ucsc.edu>
2022-04-15 11:15:57 -0700

    hey ;)

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -178,7 +178,7 @@ void addEdge(Graph G, int u, int v){
 		append(G->neighbors[u], v);
 	}
 	else{
-		for(j = 1; j < length(G->neighbors[u]); j++){
+		for(j = 1; j < length(G->neighbors[u]) + 1; j++){
 			moveFront(G->neighbors[u]);	
 			while(index(G->neighbors[u]) >= 0){
 				if(get(G->neighbors[u]) > v){
@@ -198,7 +198,7 @@ void addEdge(Graph G, int u, int v){
 		append(G->neighbors[v], u);
 	}
 	else{
-		for(j = 1; j < length(G->neighbors[v]); j++){
+		for(j = 1; j < length(G->neighbors[v]) + 1; j++){
 			moveFront(G->neighbors[v]);	
 			while(index(G->neighbors[v]) >= 0){
 				if(get(G->neighbors[v]) > u){
