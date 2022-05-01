654aa14bce079d4ca4205fdad2d62cbd8f288a85
Granger Pasko <granger@Grangers-MacBook-Pro.local>
2022-05-01 12:46:42 -0700

    -_-

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -175,6 +175,7 @@ void changeEntry(Matrix M, int i, int j, double x){
 		}
 		if(((Entry)get(M->rows[i]))->col > j && x == 0.0){
 			free(get(M->rows[i]));
+			return;
 		}	
 		moveNext(M->rows[i]);
 	}
