95cd01e4e304ca9f35fe6fe6ea9a920a29a9aab8
Granger Pasko <granger@rachelcarlson-100-64-73-60.resnet.ucsc.edu>
2022-05-17 15:34:50 -0700

    mainfile done

--- pa6/Arithmetic.cpp
+++ pa6/Arithmetic.cpp
@@ -5,8 +5,60 @@
 // Top level client file utalizing the functions built in BigIntegral to
 // perform large number arithmetic.
 //-----------------------------------------------------------------------------
+#include<iostream>
+#include<string>
+#include<fstream>
+#include"BigInteger.h"
 
 
 int main(int argc, char* argv[]){
-	return 0;
+	if(argc != 3){
+		printf("Invalid number of arguments. Use ./Arithmetic <input file> <output file>\n");
+		return EXIT_FAILURE;
+	}
+	std::ifstream input;
+	input.open(argv[1]);
+	std::string num1;
+	std::string num2;
+	std::ifstream output;
+	output.open(argv[2]);
+	if(input.is_open()){
+		std::getline(input, num1);
+		std::getline(input, num2);
+		std::getline(input, num2);
+	}
+	input.close();
+	BigInteger A = BigInteger(num1);
+	BigInteger B = BigInteger(num2);
+	std::cout << "A = " << A << std::endl;
+	std::cout << std::endl;
+	std::cout << "B = " << B << std::endl;
+	std::cout << std::endl;
+	std::cout << "A + B = " << A+B << std::endl;
+	std::cout << std::endl;
+	std::cout << "A - B = " << A-B << std::endl;
+	std::cout << std::endl;
+	std::cout << "A - A = " << A-A << std::endl;
+	std::cout << std::endl;
+	std::string c1 = "3";
+	std::string c2 = "2";
+	BigInteger C1 = BigInteger(c1);
+	BigInteger C2 = BigInteger(c2);
+	std::cout << "3A - 2B = " << C1*A - C2*B << std::endl;
+	std::cout << std::endl;
+	std::cout << "A*B = " << A*B << std::endl;
+	std::cout << std::endl;
+	std::cout << "A*A = " << A*A << std::endl;
+	std::cout << std::endl;
+	std::cout << "B*B = " << B*B << std::endl;
+	std::cout << std::endl;
+
+	c1 = "9";
+	C1 = BigInteger(c1);
+	c2 = "16";
+	C2 = BigInteger(c2);
+	std::cout << "9(A^4) + 16(B^5) = " << C1*A*A*A*A + C2*B*B*B*B*B << std::endl;
+	std::cout << std::endl;
+
+
 }
