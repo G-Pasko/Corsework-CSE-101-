e12044cb5f2075609c4cb632ac610621f5b1cc0b
Granger Pasko <granger@rachelcarlson-100-64-73-60.resnet.ucsc.edu>
2022-05-17 15:40:31 -0700

    mainfile done

--- pa6/Arithmetic.cpp
+++ pa6/Arithmetic.cpp
@@ -57,7 +57,7 @@ int main(int argc, char* argv[]){
 	C1 = BigInteger(c1);
 	c2 = "16";
 	C2 = BigInteger(c2);
-	std::cout << "9(A^4) + 16(B^5) = " << C1*A*A*A*A + C2*B*B*B*B*B << std::endl;
+	std::cout  C1*A*A*A*A + C2*B*B*B*B*B << std::endl;
 	std::cout << std::endl;
 
 
